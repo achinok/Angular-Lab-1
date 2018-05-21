922f8f72fe0f08067b1a9f066ae1871d4cb49dc2
achinok <37181010+achinok@users.noreply.github.com>
2018-05-21 10:33:58 -0400

    Add files via upload

--- toDoController.js
+++ toDoController.js
@@ -4,23 +4,27 @@
         let vm = this;
 
         this.task1 = {
-            task: "1. make dinner",
-            completed: false
+            task: "make dinner",
+            completed: false,
+            id: "1"
         }
 
         this.task2 = {
-            task: "2. go for a run",
-            completed: true 
+            task: "go for a run",
+            completed: true, 
+            id: "2"
         }
 
         this.task3 = {
-            task: "3. bake a cake",
-            completed: true
+            task: "bake a cake",
+            completed: true,
+            id: "3"
         }
 
         this.task4 = {
-            task: "4. get the mail",
-            completed: false
+            task: "get the mail",
+            completed: false,
+            id:"4"
         }
         
         vm.toDoList = [this.task1, this.task2, this.task3, this.task4];
