bdec4ed8dc54d52ebe7b7f555c387ebfbae38602
achinok <37181010+achinok@users.noreply.github.com>
2018-05-22 08:13:44 -0400

    Add files via upload

--- toDoController.js
+++ toDoController.js
@@ -1,40 +1,45 @@
 "use strict"
-
+// app.controller('sampleToDo', function ($scope) {
     function sampleToDo() {
-        let vm = this;
-
+    let vm = this;
+    vm.fish = [
         this.task1 = {
             task: "make dinner",
-            completed: false,
-            id: "1"
-        }
-
+            completed: "false",
+        },
         this.task2 = {
             task: "go for a run",
-            completed: true, 
-            id: "2"
-        }
-
+            completed: "false",
+        },
         this.task3 = {
             task: "bake a cake",
-            completed: true,
-            id: "3"
-        }
-
+            completed: "false",
+        },
         this.task4 = {
             task: "get the mail",
-            completed: false,
-            id:"4"
-        }
+            completed: "false",
+        },
+    ]; 
+        vm.toggleSelect = (fish) => {
+            fish.completed = !fish.completed;
+        },
+
+        // $scope.add = function () {
+        //     $scope.fish.push($scope.input);
+        //     $scope.input = '';
+        // },
+
+        // $scope.remove = function (index) {
+        //     $scope.fish.splice(index, 1);
+        // },
         
-        vm.toDoList = [this.task1, this.task2, this.task3, this.task4];
+vm.toDoList = [this.task1, this.task2, this.task3, this.task4];
                 // return toDoList;  if necessary just return the varible name of the funciton not the samepleToDo()
-    }
-
-
+                
+};
 
 {
-angular
-.module("app")
-.controller("sampleToDo", sampleToDo);
+    angular
+        .module("app")
+        .controller("sampleToDo", sampleToDo);
 };
